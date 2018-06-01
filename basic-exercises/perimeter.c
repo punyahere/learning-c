e532d1c81ef4390e398612f77ae90cb5adef52a5
punyavashist <punyavashist@pm.me>
2018-06-01 10:43:31 -0700

    program to find perimtere of a rectangle

---
+++ basic-exercises/perimeter.c
@@ -0,0 +1,9 @@
+//Q. Find the perimeter of a rectangle.
+//A. 
+	#include <stdio.h>
+		int main() {
+			int l, b;
+			printf("Enter the length and the breadth of the rectangle: ");
+			scanf("%d%d", &l, &b);
+			printf("The perimeter of the rectangle is: %d\n", l*b*2);
+				}	
