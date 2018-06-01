bcce63bdaa68633cb611b15c4274ba322172d33e
punyavashist <39782607+punyavashist@users.noreply.github.com>
2018-06-01 11:02:20 -0700

    Update arearect.c

--- basic-exercises/arearect.c
+++ basic-exercises/arearect.c
@@ -1,9 +1,9 @@
-//Q. FInd area of a rectangle.
+//Q. Find area of a rectangle.
 //A.
 		#include <stdio.h>
 			int main() {
 				int l, b;
 				printf("Please enter the length and the breadth: ");
 				scanf("%d%d", &l, &b);	
-				printf("The area of the rectangle is: %d", l*b);
+				printf("The area of the rectangle is: %d\n", l*b);
 					}
