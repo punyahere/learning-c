46bc5c1c5efcbd1d252e47396c02797cb2851bb1
punyavashist <punyavashist@pm.me>
2018-06-01 10:09:33 -0700

    program to find the sum of two numbers

---
+++ basic-exercises/sum.c
@@ -0,0 +1,9 @@
+//Q. Add two numbers together.
+//A. 
+	#include <stdio.h>
+		int main() {
+			int num1, num2;
+			printf("Enter two numbers: ");
+			scanf("%d%d", &num1, &num2);
+			printf("The sum of both the numbers is: %d\n", num1+num2);
+				}
