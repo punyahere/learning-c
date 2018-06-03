d6f15a1fd813956c88840fdaa6883456cf207729
Punya Vashist <punyavashist@pm.me>
2018-06-03 17:04:52 +0530

    program to convert centimeter to kilometer

---
+++ basic-exercises/lengthconvert.c
@@ -0,0 +1,9 @@
+//Q.Program to convert length in cm to km.
+//A.
+	#include <stdio.h>
+		int main() {
+			int length;
+			printf("Please enter the length: ");
+			scanf("%d", &length);
+			printf("The length in km is %d", length*1000);
+				}
